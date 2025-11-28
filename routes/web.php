f8693dce302bee8528c65821aef806f360ae50f3
Garfan Khan <garfankhan91789@aol.com>
2025-11-28 18:43:24 +0530

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/services', [PageController::class, 'services'])->name('services');
+Route::get('/portfolio', [PageController::class, 'portfolio'])->name('portfolio');
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
