95392d4fd7d948fa7b21d8b87d331d94154625a5
Garfaan Khan <garfankhan91789@aol.com>
2025-12-08 01:10:13 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
 
+// Public pages
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/services', [PageController::class, 'services'])->name('services');
@@ -11,4 +12,5 @@ Route::get('/portfolio', [PageController::class, 'portfolio'])->name('portfolio'
 Route::get('/blog', [PageController::class, 'blog'])->name('blog');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
+// Contact form submission
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
